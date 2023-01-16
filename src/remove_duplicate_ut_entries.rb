9142ec09b27dfc19330a6fb9385d5968b0b84c8c
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2023-01-17 04:37:22 +0900

    Sync

--- src/remove_duplicate_ut_entries.rb
+++ src/remove_duplicate_ut_entries.rb
@@ -4,6 +4,7 @@
 # Author: UTUMI Hirosi (utuhiro78 at yahoo dot co dot jp)
 # License: Apache License, Version 2.0
 
+require 'open-uri'
 require 'nkf'
 
 targetfiles = ARGV
@@ -12,6 +13,10 @@ if ARGV == []
 	puts "Usage: ruby script.rb [FILE]"
 	exit
 end
+
+url = "https://raw.githubusercontent.com/google/mozc/master/src/data/dictionary_oss/id.def"
+id_mozc = URI.open(url).read.split(" 名詞,一般,")[0]
+id_mozc = id_mozc.split("\n")[-1]
 
 filename = ARGV[0]
 dicname = filename
@@ -29,9 +34,13 @@ lines.length.times do |i|
 	s2 = lines[i - 1].split("	")
 
 	# UT辞書内で重複するエントリをコスト順にスキップ
+	# あいおい	1847	1847	9000	相生
 	if s1[0] + " " + s1[-1] == s2[0] + " " + s2[-1]
 		next
 	end
+
+	# IDを最新のものに変更
+	s1[1..2] = [id_mozc, id_mozc]
 
 	dicfile.puts s1.join("	")
 end
