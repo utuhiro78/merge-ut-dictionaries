691d553053e50773ea11ae274dd339c82ac83290
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2023-01-22 04:53:48 +0900

    Sync

--- src/get_latest_mozc.rb
+++ src/get_latest_mozc.rb
@@ -6,8 +6,11 @@
 
 require 'open-uri'
 
+# Fcitx のリポジトリにある Mozc を取得
+# Fcitx5-Mozc が含まれているので。
+
 # Mozc のバージョンを取得
-uri = URI.parse("https://raw.githubusercontent.com/google/mozc/master/src/data/version/mozc_version_template.bzl")
+uri = URI.parse("https://raw.githubusercontent.com/fcitx/mozc/fcitx/src/data/version/mozc_version_template.bzl")
 lines = uri.read.to_s
 
 version = lines.split('MAJOR = ')[1][0] + '.'
@@ -15,7 +18,7 @@ version = version + lines.split('MINOR = ')[1][0..1] + '.'
 version = version + lines.split('BUILD_OSS = ')[1][0..3] + '.'
 
 # Mozc の最終コミット日を取得
-uri = URI.parse("https://github.com/google/mozc/commits/master")
+uri = URI.parse("https://github.com/fcitx/mozc/commits/fcitx")
 lines = uri.read.to_s
 
 # <relative-time datetime="2023-01-16T12:00:11Z"
@@ -34,21 +37,10 @@ end
 
 # Mozc を取得
 `rm -rf mozc`
-`git clone --depth 1 --recursive --shallow-submodules https://github.com/google/mozc.git`
+`git clone --depth 1 --recursive --shallow-submodules https://github.com/fcitx/mozc.git`
 
 `rm -rf mozc/.git/`
 `mv mozc #{mozcdir}`
-
-# Fcitx5-Mozc を取得
-`wget https://github.com/fcitx/mozc/archive/refs/heads/fcitx.zip -O mozc-fcitx.zip`
-`unzip -q mozc-fcitx.zip`
-
-# 差分を Mozc にコピー
-`cp -r mozc-fcitx/scripts #{mozcdir}/`
-`rm -rf #{mozcdir}/src/unix`
-`cp -r mozc-fcitx/src/unix #{mozcdir}/src/`
-`cp -f mozc-fcitx/src/{BUILD.fcitx*.bazel,WORKSPACE.bazel} #{mozcdir}/src/`
-`rm -rf mozc-fcitx*`
 
 # Mozc のアーカイブを作成
 `tar -cjf #{mozcdir}.tar.bz2 #{mozcdir}`
