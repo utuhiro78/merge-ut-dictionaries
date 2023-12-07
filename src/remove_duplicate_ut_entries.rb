0a8ee6ed6e2389119d897d3b13f00910dc598264
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2023-12-07 15:06:01 +0900

    Add comments

--- src/remove_duplicate_ut_entries.rb
+++ src/remove_duplicate_ut_entries.rb
@@ -25,26 +25,28 @@ file = File.new(filename, "r")
 	lines = file.read.split("\n")
 file.close
 
-dicfile = File.new(dicname, "w")
-
 lines.length.times do |i|
 	s1 = lines[i].split("	")
 
 	# IDを最新のものに変更
 	s1[1..2] = [id_mozc, id_mozc]
 
-	lines[i] = [s1[0], s1[4], s1[3], s1[1],s1[2]]
-	lines[i] = lines[i].join("	")
+	# 読みと表記を先頭にする
+	s1 = [s1[0], s1[4], s1[3], s1[1],s1[2]]
+	lines[i] = s1.join("	")
 end
 
 lines = lines.sort
+
+dicfile = File.new(dicname, "w")
 
 lines.length.times do |i|
 	s1 = lines[i].split("	")
 	s2 = lines[i - 1].split("	")
 
 	# UT辞書内で重複するエントリのうち、コスト値の大きいものをスキップ
-	# あいおい	1847	1847	9000	相生
+	# あいおい	相生	1847	1847	8200
+	# あいおい	相生	1847	1847	8400
 	if s1[0..1] == s2[0..1]
 		next
 	end
