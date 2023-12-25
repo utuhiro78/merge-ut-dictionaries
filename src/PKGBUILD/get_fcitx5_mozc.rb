b1c327f437bb3dd89f72f07b0c09df86aa88ff3f
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2023-12-25 22:33:19 +0900

    Change base URL

--- src/PKGBUILD/get_fcitx5_mozc.rb
+++ src/PKGBUILD/get_fcitx5_mozc.rb
@@ -7,7 +7,7 @@
 require 'open-uri'
 
 # Mozc のバージョンを取得
-uri = URI.parse("https://raw.githubusercontent.com/fcitx/mozc/fcitx/src/data/version/mozc_version_template.bzl")
+uri = URI.parse("https://raw.githubusercontent.com/google/mozc/master/src/data/version/mozc_version_template.bzl")
 lines = uri.read.to_s
 
 version = lines.split('MAJOR = ')[1][0] + '.'
@@ -15,7 +15,7 @@ version = version + lines.split('MINOR = ')[1][0..1] + '.'
 version = version + lines.split('BUILD_OSS = ')[1][0..3] + '.'
 
 # Mozc の最終コミット日を取得
-uri = URI.parse("https://github.com/fcitx/mozc/commits/fcitx")
+uri = URI.parse("https://github.com/google/mozc/commits/master/")
 lines = uri.read.to_s
 
 # <relative-time datetime="2023-01-16T12:00:11Z"
@@ -35,8 +35,15 @@ end
 # Mozc を取得
 `rm -rf mozc`
 `git clone --depth 1 --recursive --shallow-submodules https://github.com/fcitx/mozc.git`
-
 `rm -rf mozc/.git/`
+
+# Fcitx-mozc を取得
+`wget -N https://github.com/fcitx/mozc/archive/refs/heads/fcitx.zip -O mozc-fcitx.zip`
+`7z x mozc-fcitx.zip mozc-fcitx/src/unix`
+`rm -rf mozc/src/unix`
+`mv mozc-fcitx/src/unix mozc/src/`
+`rm -rf mozc-fcitx`
+
 `mv mozc #{mozcdir}`
 
 # Mozc のアーカイブを作成
