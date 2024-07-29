1f1cdcf545b952f84fdad78d58c0db7a662b592d
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-07-30 03:42:25 +0900

    Code style fixes

--- src/apply_word_hits.py
+++ src/apply_word_hits.py
@@ -13,16 +13,11 @@ if len(sys.argv) == 1:
 
 file_name = sys.argv[1]
 
-# jawiki_hits を読み込む
-# jawiki_hits    0    0    34    中居正広
-with open("jawiki-latest-all-titles-in-ns0.hits", "r", encoding="utf-8") as file:
-    lines = file.read().splitlines()
+# jawiki_hits と mozcdic-ut を読み込む
+with open("jawiki-latest-all-titles-in-ns0.hits", "r", encoding="utf-8") as file1, \
+        open(file_name, "r", encoding="utf-8") as file2:
+    lines = file1.read().splitlines() + file2.read().splitlines()
 
-# mozcdic-ut を jawiki_hits に追加
-with open(file_name, "r", encoding="utf-8") as file:
-    lines = lines + file.read().splitlines()
-
-# 表記順に並べ替える
 for i in range(len(lines)):
     # jawiki_hits    0    0    34    中居正広
     # なかいまさひろ    1843    1843    6477    中居正広
@@ -32,6 +27,7 @@ for i in range(len(lines)):
     # 表記を正規化
     lines[i][4] = normalize("NFKC", lines[i][4])
 
+    # 表記を先頭にする
     lines[i].insert(0, lines[i][4])
     lines[i] = lines[i][0:5]
 
@@ -41,6 +37,7 @@ l2 = []
 for line in lines:
     # 中居正広    jawiki_hits    0    0    34
     # 中居正広    なかいまさひろ    1843    1843    6477
+
     line[4] = int(line[4])
 
     if line[1] == "jawiki_hits":
@@ -52,8 +49,8 @@ for line in lines:
 
         continue
 
-    # jawiki に存在しない英数字のみの表記はスキップ
-    # それ以外の英数字のみの表記はコストを 9000 台にする
+    # 表記が英数字のみで、jawiki に存在しない場合はスキップ
+    # 存在する場合はコストを 9000 台にする
     if len(line[0]) == len(line[0].encode()):
         if line[0] != line_wiki[0]:
             continue
@@ -69,6 +66,7 @@ for line in lines:
     else:
         line[4] = str(8000 - (line_wiki[4] * 10))
 
+    # Mozc 辞書の並びに戻す
     line.append(line[0])
     line = line[1:]
     l2.append(line)
