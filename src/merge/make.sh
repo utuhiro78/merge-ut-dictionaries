6bfcb3999ae928b422f83c67da4146eb29ff3d0e
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-10-27 03:40:47 +0900

    Add an option: generate_latest

--- src/merge/make.sh
+++ src/merge/make.sh
@@ -4,7 +4,7 @@
 # License: Apache License, Version 2.0
 
 #alt_cannadic="true"
-#edict="true"
+#edict2="true"
 jawiki="true"
 #neologd="true"
 personal_names="true"
@@ -12,45 +12,85 @@ place_names="true"
 #skk_jisyo="true"
 sudachidict="true"
 
-rm -f mozcdic-ut.txt
-rm -rf mozcdic-ut-*/
+#generate_latest="true"
 
-if [[ $alt_cannadic = "true" ]]; then
+rm -rf mozcdic-ut*
+
+if [[ $alt_cannadic = "true" ]] && [[ $generate_latest != "true" ]]; then
 git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-alt-cannadic.git
 fi
 
-if [[ $edict = "true" ]]; then
+if [[ $alt_cannadic = "true" ]] && [[ $generate_latest = "true" ]]; then
+cd ../alt-cannadic/
+sh make.sh
+cd -
+fi
+
+if [[ $edict2 = "true" ]] && [[ $generate_latest != "true" ]]; then
 git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-edict2.git
 fi
 
-if [[ $jawiki = "true" ]]; then
+if [[ $edict2 = "true" ]] && [[ $generate_latest = "true" ]]; then
+cd ../edict2/
+sh make.sh
+cd -
+fi
+
+if [[ $jawiki = "true" ]] && [[ $generate_latest != "true" ]]; then
 git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-jawiki.git
 fi
 
-if [[ $neologd = "true" ]]; then
+if [[ $jawiki = "true" ]] && [[ $generate_latest = "true" ]]; then
+cd ../jawiki/
+sh make.sh
+cd -
+fi
+
+if [[ $neologd = "true" ]] && [[ $generate_latest != "true" ]]; then
 git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-neologd.git
+fi
+
+if [[ $neologd = "true" ]] && [[ $generate_latest = "true" ]]; then
+cd ../neologd/
+sh make.sh
+cd -
 fi
 
 if [[ $personal_names = "true" ]]; then
 git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-personal-names.git
 fi
 
-if [[ $place_names = "true" ]]; then
+if [[ $place_names = "true" ]] && [[ $generate_latest != "true" ]]; then
 git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-place-names.git
 fi
 
-if [[ $skk_jisyo = "true" ]]; then
+if [[ $place_names = "true" ]] && [[ $generate_latest = "true" ]]; then
+cd ../place-names/
+sh make.sh
+cd -
+fi
+
+if [[ $skk_jisyo = "true" ]] && [[ $generate_latest != "true" ]]; then
 git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-skk-jisyo.git
 fi
 
-if [[ $sudachidict = "true" ]]; then
+if [[ $skk_jisyo = "true" ]] && [[ $generate_latest = "true" ]]; then
+cd ../skk-jisyo/
+sh make.sh
+cd -
+fi
+
+if [[ $sudachidict = "true" ]] && [[ $generate_latest != "true" ]]; then
 git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-sudachidict.git
 fi
 
-# UT辞書を展開して結合
-mv mozcdic-ut-*/mozcdic-ut-*.txt.tar.bz2 .
-for f in mozcdic-ut-*.txt.tar.bz2; do tar xf "$f"; done
+if [[ $sudachidict = "true" ]] && [[ $generate_latest = "true" ]]; then
+cd ../sudachidict/
+sh make.sh
+cd -
+fi
 
+for f in */mozcdic-ut-*.txt.tar.bz2; do tar xf "$f"; done
 cat mozcdic-ut-*.txt > mozcdic-ut.txt
 
 # IDを更新、重複エントリを削除、コストを調整
