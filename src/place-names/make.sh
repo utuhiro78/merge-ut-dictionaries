56fb33d2c20864e676977bdbf1afafc4aa241b25
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-10-29 22:55:15 +0900

    Delete src directory

--- src/place-names/make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Author: UTUMI Hirosi (utuhiro78 at yahoo dot co dot jp)
-# License: Apache License, Version 2.0
-
-python generate_place_names_for_mozc.py
-
-tar cjf mozcdic-ut-place-names.txt.tar.bz2 mozcdic-ut-place-names.txt
-mv mozcdic-ut-*.txt* ../merge/
