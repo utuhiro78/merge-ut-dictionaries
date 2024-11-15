11130e2e653a1437e2506c5b8de3229ed9aaf62f
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-11-15 10:15:29 +0900

    Add tabs

--- src/merge/make.sh
+++ src/merge/make.sh
@@ -17,77 +17,77 @@ sudachidict="true"
 rm -rf mozcdic-ut*
 
 if [[ $alt_cannadic = "true" ]] && [[ $generate_latest != "true" ]]; then
-git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-alt-cannadic.git
+    git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-alt-cannadic.git
 fi
 
 if [[ $alt_cannadic = "true" ]] && [[ $generate_latest = "true" ]]; then
-cd ../alt-cannadic/
-sh make.sh
-cd -
+    cd ../alt-cannadic/
+    sh make.sh
+    cd -
 fi
 
 if [[ $edict2 = "true" ]] && [[ $generate_latest != "true" ]]; then
-git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-edict2.git
+    git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-edict2.git
 fi
 
 if [[ $edict2 = "true" ]] && [[ $generate_latest = "true" ]]; then
-cd ../edict2/
-sh make.sh
-cd -
+    cd ../edict2/
+    sh make.sh
+    cd -
 fi
 
 if [[ $jawiki = "true" ]] && [[ $generate_latest != "true" ]]; then
-git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-jawiki.git
+    git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-jawiki.git
 fi
 
 if [[ $jawiki = "true" ]] && [[ $generate_latest = "true" ]]; then
-cd ../jawiki/
-sh make.sh
-cd -
+    cd ../jawiki/
+    sh make.sh
+    cd -
 fi
 
 if [[ $neologd = "true" ]] && [[ $generate_latest != "true" ]]; then
-git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-neologd.git
+    git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-neologd.git
 fi
 
 if [[ $neologd = "true" ]] && [[ $generate_latest = "true" ]]; then
-cd ../neologd/
-sh make.sh
-cd -
+    cd ../neologd/
+    sh make.sh
+    cd -
 fi
 
 if [[ $personal_names = "true" ]]; then
-git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-personal-names.git
+    git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-personal-names.git
 fi
 
 if [[ $place_names = "true" ]] && [[ $generate_latest != "true" ]]; then
-git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-place-names.git
+    git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-place-names.git
 fi
 
 if [[ $place_names = "true" ]] && [[ $generate_latest = "true" ]]; then
-cd ../place-names/
-sh make.sh
-cd -
+    cd ../place-names/
+    sh make.sh
+    cd -
 fi
 
 if [[ $skk_jisyo = "true" ]] && [[ $generate_latest != "true" ]]; then
-git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-skk-jisyo.git
+    git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-skk-jisyo.git
 fi
 
 if [[ $skk_jisyo = "true" ]] && [[ $generate_latest = "true" ]]; then
-cd ../skk-jisyo/
-sh make.sh
-cd -
+    cd ../skk-jisyo/
+    sh make.sh
+    cd -
 fi
 
 if [[ $sudachidict = "true" ]] && [[ $generate_latest != "true" ]]; then
-git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-sudachidict.git
+    git clone --depth 1 https://github.com/utuhiro78/mozcdic-ut-sudachidict.git
 fi
 
 if [[ $sudachidict = "true" ]] && [[ $generate_latest = "true" ]]; then
-cd ../sudachidict/
-sh make.sh
-cd -
+    cd ../sudachidict/
+    sh make.sh
+    cd -
 fi
 
 bzip2 -dfk mozcdic-ut-*/mozcdic-ut-*.txt.bz2
