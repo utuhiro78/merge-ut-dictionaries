07540f08fb993ef83fb7f6b2240efb3d09afe024
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2025-02-06 15:46:19 +0900

    Delete PKGBUILD/restart-fcitx5.sh

--- PKGBUILD/restart-fcitx5.sh
+++
@@ -1,9 +0,0 @@
-rm -rf {pkg,src}
-killall fcitx5 mozc_server
-fcitx5 &
-
-printf 'export GTK_IM_MODULE=fcitx
-export QT_IM_MODULE=fcitx
-export XMODIFIERS=@im=fcitx' > ~/.xprofile
-
-#rm -rf ~/.cache/bazel/
