6ef1eb8f67148137ef3550796c93f1490e35999f
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2026-04-03 06:13:00 +0900

    Refactoring

--- src/jawiki/convert_jawiki.py
+++ src/jawiki/convert_jawiki.py
@@ -7,88 +7,150 @@
 import bz2
 import html
 import jaconv
+import multiprocessing
 import re
-import subprocess
-from multiprocessing import Pool, cpu_count
+import urllib.request
+import xml.etree.ElementTree as ET
+from pathlib import Path
 from unicodedata import normalize
+
+# 「}}'''」にマッチ
+RE_HYOUKI_AFTER_TEMPLATE = re.compile(r'\}\}(?=\'\'\'|「|『)')
+# 「{{...}}」にマッチ
+RE_TEMPLATE = re.compile(r'{{.*?}}')
+# 「<ref .../>」と「<ref>...</ref>」にマッチ
+RE_REF = re.compile(r'<ref.*?(/>|>.*?</ref>)', re.DOTALL)
+# 「)、/{\[,」のいずれか1文字にマッチ
+RE_YOMI_KUGIRI = re.compile(r'[)、/{\[,]')
+
+# ひらがなとカタカナと長音のいずれか1文字にマッチ
+RE_HIRAGANA_KATAKANA = re.compile(r'[ぁ-ゔァ-ヴー]')
+# カタカナと長音のいずれか1文字にマッチ
+RE_KATAKANA = re.compile(r'[ァ-ヴー]')
+
+# 「 \'"「」『』」を削除
+TRANS_KAKKO = str.maketrans('', '', ' \'"「」『』')
+# 「,.!?-+*=:・、。×★☆」を削除
+TRANS_NON_YOMI = str.maketrans('', '', ',.!?-+*=:・、。×★☆')
+# 「ゐ」「ゑ」を「い」「え」に置換
+TRANS_OLD_I_E = str.maketrans('ゐゑ', 'いえ', '')
 
 
 def main():
-    jawiki_dict = get_articles()
+    jawiki_dict = generate_jawiki_dict()
 
     with open('mozcdic-ut-jawiki.txt', 'w', encoding='utf-8') as file:
-        file.writelines(jawiki_dict)
+        for entry in jawiki_dict:
+            entry = f'{entry[0]}\t0000\t0000\t8000\t{entry[1]}\n'
+            file.write(entry)
 
 
-def get_articles():
-    # Wikipedia のダンプを取得
-    subprocess.run(
-        ['wget', '-N', 'https://dumps.wikimedia.org/jawiki/latest/' +
-            'jawiki-latest-pages-articles-multistream.xml.bz2'])
+def generate_jawiki_dict():
+    # jawiki-*-articles-multistream.xml.bz2 を取得
+    url = 'https://dumps.wikimedia.org/jawiki/latest/' + \
+        'jawiki-latest-pages-articles-multistream.xml.bz2-rss.xml'
 
-    buffer = 100 * 1024 ** 2
-    article_part = ''
-    cpu_cores = cpu_count()
-    jawiki_dict = []
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+        Chrome/91.0.4472.124"}
+
+    try:
+        req = urllib.request.Request(url, headers=headers)
+        with urllib.request.urlopen(req) as response:
+            root = ET.fromstring(response.read().decode())
+    except urllib.error.HTTPError as e:
+        print(f'Error: {e.code}')
+
+    description_text = root.find('.//item/description').text
+    url = re.search(r'href="([^"]+)"', description_text).group(1)
+
+    jawiki_articles_file = url.rsplit('/', 1)[1]
+
+    if not Path(jawiki_articles_file).exists():
+        try:
+            req = urllib.request.Request(url, headers=headers)
+            with urllib.request.urlopen(req) as response, \
+                    open(jawiki_articles_file, "wb") as out_file:
+                # 1MB ずつ書き込む
+                while True:
+                    chunk = response.read(1024 * 1024)
+                    if not chunk:
+                        break
+                    out_file.write(chunk)
+        except urllib.error.HTTPError as e:
+            print(f'Error: {e.code}')
 
     # Wikipedia のダンプを読み込む
-    with bz2.open(
-        'jawiki-latest-pages-articles-multistream.xml.bz2', 'rt',
-            encoding='utf-8') as file:
-        while True:
-            articles = file.read(buffer)
-
-            # 最後まで読んだら終了
-            if articles == '':
-                break
-
-            articles = articles.split('  </page>')
-            articles[0] = article_part + articles[0]
-
-            # ページの断片を保存
-            article_part = articles[-1]
-
-            with Pool(processes=cpu_cores) as p:
-                jawiki_dict += p.map(get_hyouki, articles[:-1])
-
+    jawiki_dict = []
     c = 0
 
-    for line in jawiki_dict:
-        if type(line) is not list:
-            continue
+    # プロセスプールを作成
+    with multiprocessing.Pool() as pool:
+        for result in pool.imap(
+                get_dict_entry,
+                get_jawiki_article(jawiki_articles_file),
+                chunksize=100):
+            if result:
+                jawiki_dict.append(result)
+                if c % 1000 == 0:
+                    print(f'{c}: {result}')
 
-        jawiki_dict[c] = f'{line[0]}\t0000\t0000\t8000\t{line[1]}\n'
-        c += 1
+                c += 1
 
-    jawiki_dict = jawiki_dict[:c]
-
-    # 重複する行を削除
+    # 重複するエントリを削除
     jawiki_dict = sorted(list(set(jawiki_dict)))
+    return jawiki_dict
 
-    return (jawiki_dict)
+
+def get_jawiki_article(jawiki_articles_file):
+    with bz2.open(jawiki_articles_file, 'rt', encoding='utf-8') as file:
+        context = ET.iterparse(file, events=('start', 'end'))
+        event, root = next(context)
+
+        try:
+            for event, elem in context:
+                if event == 'end' and elem.tag.endswith('page'):
+                    title = elem.findtext('{*}title')
+                    text_elem = elem.find('{*}revision/{*}text')
+                    article = text_elem.text or ""
+                    yield (title, article)
+                    elem.clear()
+                    root.clear()
+
+        except ET.ParseError:
+            yield ('', '')
 
 
-def get_hyouki(article):
-    # title から表記を取得
-    #     <title>あいの里公園駅</title>
-    article = article.split('</title>')
+def get_dict_entry(data):
+    title, article = data
 
-    if len(article) == 1:
-        return
+    hyouki = get_hyouki(title)
+    if not hyouki:
+        return None
 
-    hyouki = article[0].split('<title>')[1]
+    yomi = get_yomi(hyouki, article)
+    if not yomi:
+        return None
 
-    # 記事を取得
-    article = article[1].split('xml:space="preserve">')
+    return (yomi, hyouki)
 
-    if len(article) == 1:
-        return
 
-    article = article[1]
+def get_hyouki(title):
+    # title を最後の「 (」で切って表記にする
+    #     スティル・ライフ (ローリング・ストーンズのアルバム)
+    hyouki = title.rsplit(' (', 1)[0]
 
-    # 表記を「 (」で切る
-    #     田中瞳 (アナウンサー)
-    hyouki = hyouki.split(' (')[0]
+    # 表記にスペースがある場合はスキップ
+    #     記事のスペースを削除して「表記(読み)」を検索するので。
+    # 表記に「、」がある場合はスキップ
+    #     「明日、キミと」などが1語として変換されてしまうので。
+    if ' ' in hyouki or \
+            '、' in hyouki:
+        return None
+
+    hyouki = remove_short_or_long_hyouki(hyouki)
+    if not hyouki:
+        return None
 
     # 全角英数を半角に変換
     hyouki = normalize('NFKC', hyouki)
@@ -96,40 +158,29 @@ def get_hyouki(article):
     # HTMLの特殊文字を変換
     hyouki = html.unescape(hyouki)
 
-    # 表記にスペースがある場合はスキップ
-    #     記事のスペースを削除して「表記(読み)」を検索するので、
-    #     残してもマッチしない
-    # 表記が26文字以上の場合はスキップ。候補ウィンドウが大きくなりすぎる
-    if ' ' in hyouki or \
-            len(hyouki) > 25:
-        return
-
-    jawiki_dict = get_yomi(hyouki, article)
-    return (jawiki_dict)
+    return hyouki
 
 
 def get_yomi(hyouki, article):
-    # 表記から記号を削除して hyouki2 を作る
-    hyouki2 = remove_kigou(hyouki)
+    # 読みにならない文字を削除して hyouki_strip を作る
+    hyouki_strip = hyouki.translate(TRANS_NON_YOMI)
 
-    # hyouki2 が1文字以下の場合はスキップ
-    if len(hyouki2) < 2:
-        return
+    # hyouki_strip が1文字以下の場合はスキップ
+    if len(hyouki_strip) < 2:
+        return None
 
-    # hyouki2 がひらがなカタカナのみの場合は、読みを hyouki2 から作る
+    # hyouki_strip がひらがなカタカナのみの場合は、
+    # 読みを hyouki_strip から作る
     #     さいたまスーパーアリーナ
-    if hyouki2 == collect_hiragana_katakana(hyouki2):
-        yomi = convert_to_hiragana(hyouki2)
-        jawiki_dict = [yomi, hyouki]
-        return (jawiki_dict)
+    if hyouki_strip == collect_hiragana_katakana(hyouki_strip):
+        yomi = convert_to_hiragana(hyouki_strip)
+        return yomi
 
     # 記事の最初の見出し以降を削除
     lines = article.split('\n==')[0]
 
-    # テンプレート末尾と記事本文の間に改行を入れる
-    lines = lines.replace('}}\'\'\'', '}}\n\'\'\'')
-    lines = lines.replace('}}「\'\'\'', '}}\n\'\'\'')
-    lines = lines.replace('}}『\'\'\'', '}}\n\'\'\'')
+    # 「}}'''」を「}}\n'''」に置換
+    lines = RE_HYOUKI_AFTER_TEMPLATE.sub('}}\n\'\'\'', lines)
 
     # 「{{読み仮名」の前に改行を入れる
     lines = lines.replace('{{読み仮名', '\n{{読み仮名')
@@ -140,20 +191,16 @@ def get_yomi(hyouki, article):
         # 「{{読み仮名|表記|読み}}」から「表記(読み)」を作成
         #     {{読み仮名_ruby不使用|エコーチェンバー現象|
         #     エコーチェンバーげんしょう}}とは、
-        if line[:6] == '{{読み仮名':
-            l2 = line.split('|')
-
-            if len(l2) > 2:
-                l2[2] = l2[2].split('}}')[0]
-                line = f'{l2[1]}({l2[2]}) {line}'
+        if line.startswith('{{読み仮名'):
+            line_mod = line.split('|')
+            if len(line_mod) > 2:
+                line_mod[2] = line_mod[2].split('}}')[0]
+                line = f'{line_mod[1]}({line_mod[2]}) {line}'
 
         # 収録語は「盛夏(せいか)」が最小なので、7文字以下の行をスキップ
         # テンプレートをスキップ
         if len(line) < 8 or \
-                line[0] == '{' or \
-                line[0] == ' ' or \
-                line[0] == '|' or \
-                line[0] == '*':
+                line[0] in ('{', ' ', '|', '*'):
             continue
 
         # 全角英数を半角に変換
@@ -162,16 +209,13 @@ def get_yomi(hyouki, article):
         # HTMLの特殊文字を変換
         line = html.unescape(line)
 
-        # '{{' から '}}' までの最短一致を削除
-        # '<ref' から '/ref>' までの最短一致を削除
-        # '<ref name' から '/>' までの最短一致を削除
-        line = re.sub(r'{{.*?}}', '', line)
-        line = re.sub(r'<ref.*?/ref>', '', line)
-        line = re.sub(r'<ref name.*?/>', '', line)
+        # 「{{...}}」を削除
+        line = RE_TEMPLATE.sub('', line)
+        # 「<ref .../>」と「<ref>...</ref>」を削除
+        line = RE_REF.sub('', line)
 
-        # ' \'"「」『』' を削除
-        table = str.maketrans('', '', ' \'"「」『』')
-        line = line.translate(table)
+        # 「 \'"「」『』」を削除
+        line = line.translate(TRANS_KAKKO)
 
         # 「表記(読み)」から読みを取得
         line = line.split(f'{hyouki}(')
@@ -185,15 +229,14 @@ def get_yomi(hyouki, article):
         # 読みが「-」で始まる場合はスキップ
         #     スロバキア共和国(-きょうわこく
         if len(yomi) < 3 or \
-                yomi[0] == '-':
+                yomi.startswith('-'):
             continue
 
-        # 読みを ')、/{[,' で切る
-        yomi = yomi.split(')')[0].split('、')[0].split('/')[0].\
-            split('{')[0].split('[')[0].split(',')[0]
+        # 読みを「)、/{[,」で切る
+        yomi = RE_YOMI_KUGIRI.split(yomi)[0]
 
-        # 読みから記号を削除
-        yomi = remove_kigou(yomi)
+        # 読みにならない文字を削除
+        yomi = yomi.translate(TRANS_NON_YOMI)
 
         # 読みが2文字以下の場合はスキップ
         # 読みが「ー」で始まる場合はスキップ
@@ -201,7 +244,7 @@ def get_yomi(hyouki, article):
         #     ミュージシャン一覧(グループ)
         # 読みがひらがなカタカナ以外を含む場合はスキップ
         if len(yomi) < 3 or \
-                yomi[0] == 'ー' or \
+                yomi.startswith('ー') or \
                 yomi == collect_katakana(yomi) or \
                 yomi != collect_hiragana_katakana(yomi):
             continue
@@ -209,29 +252,33 @@ def get_yomi(hyouki, article):
         # 読みのカタカナをひらがなに変換
         yomi = convert_to_hiragana(yomi)
 
-        jawiki_dict = [yomi, hyouki]
-        return (jawiki_dict)
+        return yomi
 
 
-def remove_kigou(entry):
-    entry = entry.translate(str.maketrans('', '', ',.!?-+*=:・、。×★☆'))
-    return (entry)
+def collect_hiragana_katakana(entry):
+    entry = ''.join(RE_HIRAGANA_KATAKANA.findall(entry))
+    return entry
 
 
 def convert_to_hiragana(entry):
     entry = jaconv.kata2hira(entry)
-    entry = entry.translate(str.maketrans('ゐゑ', 'いえ', ''))
-    return (entry)
-
-
-def collect_hiragana_katakana(entry):
-    entry = ''.join(re.findall('[ぁ-ゔァ-ヴー]', entry))
-    return (entry)
+    entry = entry.translate(TRANS_OLD_I_E)
+    return entry
 
 
 def collect_katakana(entry):
-    entry = ''.join(re.findall('[ァ-ヴー]', entry))
-    return (entry)
+    entry = ''.join(RE_KATAKANA.findall(entry))
+    return entry
+
+
+def remove_short_or_long_hyouki(hyouki):
+    # 表記が1文字の場合はスキップ
+    # 表記が26文字以上の場合はスキップ。候補ウィンドウが大きくなりすぎる
+    len_hyouki = len(hyouki)
+    if len_hyouki < 2 or len_hyouki > 25:
+        hyouki = None
+
+    return hyouki
 
 
 if __name__ == '__main__':
