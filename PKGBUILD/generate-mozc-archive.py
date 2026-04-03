3e541f8b912f803d25915dc7a1b2bba659a88938
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2026-04-04 03:13:54 +0900

    Refactoring

--- PKGBUILD/generate-mozc-archive.py
+++ PKGBUILD/generate-mozc-archive.py
@@ -4,36 +4,40 @@
 # Author: UTUMI Hirosi (utuhiro78 at yahoo dot co dot jp)
 # License: Apache License, Version 2.0
 
-import os
+import json
 import re
 import shutil
 import subprocess
+import sys
 import urllib.request
+from datetime import datetime
+from pathlib import Path
+
+HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
 
 def main():
     # mozc のバージョンを取得
     url = 'https://raw.githubusercontent.com/' + \
             'google/mozc/refs/heads/master/src/version.bzl'
-
-    with urllib.request.urlopen(url) as response:
+    req = urllib.request.Request(url, headers=HEADERS)
+    with urllib.request.urlopen(req) as response:
         lines = response.read().decode()
 
-    version = lines.split('MAJOR = ')[1].split('\n')[0] + '.'
-    version += lines.split('MINOR = ')[1].split('\n')[0] + '.'
-    version += lines.split('BUILD_OSS = ')[1].split('\n')[0] + '.102'
+    mozc_ver = lines.split('MAJOR = ')[1].split('\n')[0] + '.'
+    mozc_ver += lines.split('MINOR = ')[1].split('\n')[0] + '.'
+    mozc_ver += lines.split('BUILD_OSS = ')[1].split('\n')[0] + '.102'
 
-    # mozc の最終コミット日を取得
-    url = 'https://github.com/google/mozc/commits/master/'
+    # Mozc の最終コミット日を取得
+    url = 'https://api.github.com/repos/google/mozc/commits/master'
     committed_date = get_committed_date(url)
 
     # mozc のアーカイブが古い場合は取得
-    mozc_ver = f'{version}.{committed_date}'
-    mozc_dir = f'mozc-{mozc_ver}'
-    fcitx_mozc_dir = f'fcitx-mozc-{mozc_ver}'
+    mozc_ver_full = f'{mozc_ver}.{committed_date}'
+    mozc_dir = f'mozc-{mozc_ver_full}'
 
-    if not os.path.exists(f'{mozc_dir}.tar.zst'):
-        if os.path.exists('mozc'):
+    if not Path(f'{mozc_dir}.tar.zst').exists():
+        if Path('mozc').exists():
             shutil.rmtree('mozc')
 
         subprocess.run(
@@ -42,44 +46,48 @@ def main():
         shutil.rmtree('mozc/.git')
         shutil.rmtree('mozc/.github')
 
-        if os.path.exists(mozc_dir):
+        if Path(mozc_dir).exists():
             shutil.rmtree(mozc_dir)
 
-        os.replace('mozc', mozc_dir)
+        Path('mozc').replace(mozc_dir)
     else:
         print(f'{mozc_dir} is up to date.')
-        exit()
+        sys.exit()
 
-    # fcitx-mozc を取得
-    if not os.path.exists(f'{fcitx_mozc_dir}.zip'):
-        subprocess.run(
-            ['wget', 'https://github.com/' +
-                'fcitx/mozc/archive/refs/heads/fcitx.zip',
-                '-O', f'{fcitx_mozc_dir}.zip'], check=True)
-    else:
-        print(f'{fcitx_mozc_dir} is up to date.')
+    fcitx_file = 'fcitx-mozc.zip'
 
-    # fcitx-mozc を展開
-    if os.path.exists(fcitx_mozc_dir):
-        shutil.rmtree(fcitx_mozc_dir)
+    # 既存の 'fcitx-mozc.zip' を削除
+    Path(fcitx_file).unlink(missing_ok=True)
 
-    shutil.unpack_archive(f'{fcitx_mozc_dir}.zip')
-    os.replace('mozc-fcitx', fcitx_mozc_dir)
+    # 'fcitx-mozc.zip' を取得
+    subprocess.run(
+        ['wget', 'https://github.com/' +
+            'fcitx/mozc/archive/refs/heads/fcitx.zip',
+            '-O', fcitx_file], check=True)
 
-    # fcitx5-mozc のファイルを mozc に移動
-    os.replace(f'{fcitx_mozc_dir}/scripts', f'{mozc_dir}/scripts')
+    fcitx_dir = 'mozc-fcitx'
+
+    # 既存の 'mozc-fcitx' を削除
+    if Path(fcitx_dir).exists():
+        shutil.rmtree(fcitx_dir)
+
+    # 'fcitx-mozc.zip' を展開
+    shutil.unpack_archive(fcitx_file)
+
+    # fcitx-mozc のファイルを mozc に移動
+    Path(f'{fcitx_dir}/scripts').replace(f'{mozc_dir}/scripts')
 
     shutil.rmtree(f'{mozc_dir}/src/unix')
-    os.replace(f'{fcitx_mozc_dir}/src/unix', f'{mozc_dir}/src/unix')
+    Path(f'{fcitx_dir}/src/unix').replace(f'{mozc_dir}/src/unix')
 
-    os.replace(
-        f'{fcitx_mozc_dir}/src/MODULE.bazel',
+    Path(
+        f'{fcitx_dir}/src/MODULE.bazel').replace(
         f'{mozc_dir}/src/MODULE.bazel')
-    os.replace(
-        f'{fcitx_mozc_dir}/src/session/BUILD.bazel',
+    Path(
+        f'{fcitx_dir}/src/session/BUILD.bazel').replace(
         f'{mozc_dir}/src/session/BUILD.bazel')
 
-    shutil.rmtree(fcitx_mozc_dir)
+    shutil.rmtree(fcitx_dir)
 
     # mozc のアーカイブを作成
     subprocess.run(
@@ -88,31 +96,34 @@ def main():
     shutil.rmtree(mozc_dir)
 
     # PKGBUILD を更新
-    update_pkgbuild('fcitx5-mozc-ut.PKGBUILD', mozc_ver)
-    update_pkgbuild('ibus-mozc-ut.PKGBUILD', mozc_ver)
+    update_pkgbuild('fcitx5-mozc-ut.PKGBUILD', mozc_ver_full)
+    update_pkgbuild('ibus-mozc-ut.PKGBUILD', mozc_ver_full)
 
 
 # GitHub の最終コミット日を取得
 def get_committed_date(url):
-    with urllib.request.urlopen(url) as response:
-        lines = response.read().decode()
+    req = urllib.request.Request(url, headers=HEADERS)
+    with urllib.request.urlopen(req) as response:
+        data = json.loads(response.read().decode())
+        date_str = data['commit']['committer']['date']
 
-    # "committedDate":"2024-01-16T06:05:57.000Z"
-    committed_date = lines.split('"committedDate":"')[1]
-    # 数字以外を削除
-    committed_date = re.sub(r'\D', '', committed_date)[:8]
-    return (committed_date)
+    date_str = datetime.fromisoformat(date_str)
+    date_str = date_str.strftime('%Y%m%d')
+
+    return date_str
 
 
 # PKGBUILD を更新
-def update_pkgbuild(pkgbuild, mozc_ver):
+def update_pkgbuild(pkgbuild, mozc_ver_full):
     with open(pkgbuild, 'r') as file:
         lines = file.read()
 
-    lines = re.sub('_mozcver=.*\n', f'_mozcver={mozc_ver}\n', lines)
+    lines = re.sub('_mozcver=.*\n', f'_mozcver={mozc_ver_full}\n', lines)
 
     with open(pkgbuild, 'w') as file:
         file.write(lines)
+
+    return
 
 
 if __name__ == '__main__':
