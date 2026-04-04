474203fca62db6a91e6371f09368fb2851b03bac
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2026-04-05 06:13:21 +0900

    Refactoring

--- src/skk-jisyo/convert_skk_jisyo.py
+++ src/skk-jisyo/convert_skk_jisyo.py
@@ -7,54 +7,80 @@
 import urllib.request
 from unicodedata import normalize
 
-urllib.request.urlretrieve(
-    'https://github.com/skk-dev/dict/raw/refs/heads/master/SKK-JISYO.L',
-    'SKK-JISYO.L')
 
-with open('SKK-JISYO.L', 'r', encoding='EUC-JP') as file:
-    lines = file.read().splitlines()
+def main():
+    urllib.request.urlretrieve(
+        'https://github.com/skk-dev/dict/raw/refs/heads/master/SKK-JISYO.L',
+        'SKK-JISYO.L')
 
-l2 = []
+    skk_jisyo = []
 
-for i in range(len(lines)):
+    with open('SKK-JISYO.L', 'r', encoding='EUC-JP') as file:
+        for line in file:
+            if not line or line.startswith(';'):
+                continue
+
+            entry = generate_dict_entry(line)
+            if entry:
+                skk_jisyo.extend(entry)
+
+    skk_jisyo = sorted(skk_jisyo)
+
+    with open('mozcdic-ut-skk-jisyo.txt', 'w', encoding='utf-8') as file:
+        for entry in skk_jisyo:
+            yomi, cost, hyouki = entry
+            entry = [yomi, '0000', '0000', cost, hyouki]
+            file.writelines(f'{"\t".join(entry)}\n')
+
+
+def generate_dict_entry(line):
     # わりふr /割り振/割振/
     # いずみ /泉/和泉;地名,大阪/出水;地名,鹿児島/
-    entry = lines[i].split(' /')
-    yomi = entry[0].replace('う゛', 'ゔ')
+    if ' /' not in line:
+        return None
+
+    entry = line.split(' /', 1)
+    yomi, hyouki = entry
+
+    yomi = yomi.replace('う゛', 'ゔ')
 
     # 読みが2文字以下の場合はスキップ
     # 読みが英数字を含む場合はスキップ
     if len(yomi) < 3 or \
             len(yomi.encode('utf-8')) != len(yomi) * 3:
-        continue
+        return None
 
-    hyouki = entry[1].split('/')
+    hyouki_set = hyouki.split('/')
+    hyouki_prev = ''
+    entry_mod = []
+    c = 0
 
-    for c in range(len(hyouki)):
-        hyouki[c] = hyouki[c].split(';')[0]
+    for hyouki in hyouki_set:
+        hyouki = hyouki.split(';')[0]
 
         # 表記の全角英数を半角に変換
-        hyouki[c] = normalize('NFKC', hyouki[c])
+        hyouki = normalize('NFKC', hyouki)
 
         # 表記が1文字の場合はスキップ
         # 表記が英数字のみの場合はスキップ
-        if len(hyouki[c]) < 2 or \
-                len(hyouki[c].encode('utf-8')) == len(hyouki[c]):
+        if len(hyouki) < 2 or \
+                len(hyouki.encode('utf-8')) == len(hyouki):
             continue
 
         # 1つ前の表記と同じ場合はスキップ
         # ＩＣカード/ICカード/
-        if hyouki[c] == hyouki[c - 1]:
+        if hyouki == hyouki_prev:
             continue
 
-        # 2個目以降の表記のコストを上げる
+        # 2個目以降の表記はコストを上げる
         cost = 8000 + (10 * c)
 
-        entry = [yomi, '0000', '0000', str(cost), hyouki[c]]
-        l2.append('\t'.join(entry) + '\n')
+        entry_mod.append([yomi, str(cost), hyouki])
+        hyouki_prev = hyouki
+        c += 1
 
-lines = sorted(set(l2))
-dict_name = 'mozcdic-ut-skk-jisyo.txt'
+    return entry_mod
 
-with open(dict_name, 'w', encoding='utf-8') as file:
-    file.writelines(lines)
+
+if __name__ == '__main__':
+    main()
