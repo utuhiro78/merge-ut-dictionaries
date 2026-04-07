aae6becbc0eb7647a0803fdceeb1a485d716576c
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2026-04-08 01:20:27 +0900

    Refactoring

--- src/alt-cannadic/convert_alt_cannadic.py
+++ src/alt-cannadic/convert_alt_cannadic.py
@@ -4,89 +4,118 @@
 # Author: UTUMI Hirosi (utuhiro78 at yahoo dot co dot jp)
 # License: Apache License, Version 2.0
 
+import io
 import re
 import tarfile
 import urllib.request
 from unicodedata import normalize
 
-urllib.request.urlretrieve(
-        'https://ftp.iij.ad.jp/pub/osdn.jp/alt-cannadic/50881/' +
-        'alt-cannadic-110208.tar.bz2', 'alt-cannadic-110208.tar.bz2')
+# ひらがなと長音にマッチ
+RE_HIRAGANA = re.compile(r'[ぁ-ゔー]+')
+# '_4小野坂_3昌也' を '小野坂昌也' に変換
+RE_JINMEI_YOMISUU = re.compile(r'[_0-9]')
 
-with tarfile.open('alt-cannadic-110208.tar.bz2') as tar:
-    file = tar.extractfile('alt-cannadic-110208/gcanna.ctd')
-    lines = file.read().decode('euc_jp')
-    file = tar.extractfile('alt-cannadic-110208/g_fname.ctd')
-    lines += file.read().decode('euc_jp')
 
-lines = lines.splitlines()
+def main():
+    alt_canna_file = 'alt-cannadic-110208.tar.bz2'
+    url = 'https://ftp.iij.ad.jp/pub/osdn.jp/alt-cannadic/50881/' + \
+        f'{alt_canna_file}'
 
-l2 = []
+    urllib.request.urlretrieve(url, alt_canna_file)
+    alt_canna = []
 
-for line in lines:
-    line = line.split(' ')
+    with tarfile.open(alt_canna_file) as tar:
+        files = [
+            'alt-cannadic-110208/gcanna.ctd',
+            'alt-cannadic-110208/g_fname.ctd']
+        for file in files:
+            f = tar.extractfile(file)
+            with io.TextIOWrapper(f, encoding='euc_jp') as text_file:
+                for entry in text_file:
+                    entry = entry.strip().split(' ')
+                    entry = generate_dict_entry(entry)
+                    if entry:
+                        alt_canna.extend(entry)
 
-    # あきびん #T35*202 空き瓶 空瓶 #T35*151 空きビン 空ビン
-    yomi = line[0]
+    alt_canna = remove_duplicate(alt_canna)
+
+    with open('mozcdic-ut-alt-cannadic.txt', 'w', encoding='utf-8') as file:
+        for entry in alt_canna:
+            file.write(f'{"\t".join(entry)}\n')
+
+
+def generate_dict_entry(entry):
+    # あきびん, #T35*202, 空き瓶, 空瓶, #T35*151, 空きビン, 空ビン
+    yomi = entry[0]
     yomi = yomi.replace('う゛', 'ゔ')
 
-    # 読みがひらがな以外を含む場合はスキップ
     # 読みが2文字以下の場合はスキップ
-    if yomi != ''.join(re.findall('[ぁ-ゔー]', yomi)) or \
-            len(yomi) < 3:
-        continue
+    # 読みがひらがな以外を含む場合はスキップ
+    if len(yomi) < 3 or \
+            not RE_HIRAGANA.fullmatch(yomi):
+        return None
 
-    hinsi = ''
-    cost_anthy = 0
+    id = '0000'
+    c = 1
 
     # 読みを除去したエントリを作る
-    line = line[1:]
+    entry = entry[1:]
+    entry_mod = []
 
-    for c in range(len(line)):
+    for entry_part in entry:
         # 「#」で始まるエントリの場合は品詞とコストを取得
         #     #T35*202 空き瓶 空瓶 #T35*151 空きビン 空ビン
-        if line[c][0] == '#':
-            entry = line[c].split('*')
-            hinsi = entry[0]
-            cost_anthy = int(entry[1])
+        if entry_part.startswith('#'):
+            if entry_part.startswith('#T3') or \
+                    entry_part.startswith('#T0') or \
+                    entry_part.startswith('#JN') or \
+                    entry_part.startswith('#KK') or \
+                    entry_part.startswith('#CN'):
+                cost = entry_part.split('*', 1)[1]
+                cost = (9000 - int(cost)) + c
+                c += 2
+                id = '0000'
+                continue
+            elif entry_part.startswith('#_'):
+                entry_part = entry_part[1:]
+                entry_part = RE_JINMEI_YOMISUU.sub('', entry_part)
+            else:
+                id = None
+
+        if not id:
             continue
 
-        hyouki = normalize('NFKC', line[c])
-
+        hyouki = entry_part
         # 表記が1文字以下の場合はスキップ
         if len(hyouki) < 2:
             continue
 
-        # alt-cannadic のコストから Mozc 辞書のコストを作る
-        #     「#T35*202 空き瓶 空瓶 #T35*151 空きビン 空ビン」の場合、
-        #     「空き瓶 空瓶 空きビン 空ビン」の順に優先されるようにする
-        # Mozc 辞書のコストは 8000 台にする
-        cost_mozc = (9000 - cost_anthy) + c
+        hyouki = normalize('NFKC', hyouki)
+        entry_mod.append([yomi, cost, hyouki])
 
-        # 収録する品詞を選択
-        if hinsi.startswith('#T3') or \
-                hinsi.startswith('#T0') or \
-                hinsi.startswith('#JN') or \
-                hinsi.startswith('#KK') or \
-                hinsi.startswith('#CN'):
-            entry = [yomi, hyouki, str(cost_mozc)]
-            l2.append(entry)
+    return entry_mod
 
-lines = sorted(l2)
-l2 = []
 
-for i in range(len(lines)):
-    # [読み, 表記] が重複するエントリをスキップ
-    #     [yomi, hyouki, str(cost_mozc)]
-    if lines[i][:2] == lines[i - 1][:2]:
-        continue
+def remove_duplicate(alt_canna):
+    # yomi -> hyouki -> cost の優先順でソート
+    alt_canna.sort(key=lambda x: (x[0], x[2], x[1]))
+    alt_canna_mod = []
+    prev_key = ()
 
-    # Mozc 辞書の並びに変更
-    entry = [lines[i][0], '0000', '0000', lines[i][2], lines[i][1]]
-    l2.append('\t'.join(entry) + '\n')
+    for entry in alt_canna:
+        yomi, cost, hyouki = entry
+        current_key = (yomi, hyouki)
 
-lines = l2
-dict_name = 'mozcdic-ut-alt-cannadic.txt'
+        # 重複する UT エントリを削除
+        # Mozc 形式のエントリを作成
+        if prev_key != current_key:
+            entry = [yomi, '0000', '0000', str(cost), hyouki]
+            alt_canna_mod.append(entry)
 
-with open(dict_name, 'w', encoding='utf-8') as file:
-    file.writelines(lines)
+        prev_key = current_key
+
+    return alt_canna_mod
+
+
+if __name__ == '__main__':
+    main()
